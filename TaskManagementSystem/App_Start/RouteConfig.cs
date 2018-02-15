bf462745aa552543f322653fe106274325c3349f
Laxmilal Janwa <laxmilal.janwa@hotmail.com>
2018-02-15 19:57:10 +0530

    DeshBoard -SubPage Call

--- TaskManagementSystem/App_Start/RouteConfig.cs
+++ TaskManagementSystem/App_Start/RouteConfig.cs
@@ -12,7 +12,11 @@ namespace TaskManagementSystem
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapRoute(
+               name: "leave",
+               url: "Leave",
+               defaults: new { controller = "Leave", action = "Create", id = UrlParameter.Optional }
+           );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
