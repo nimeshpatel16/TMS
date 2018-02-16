17c4a463cb5cce9cbaaf23b63f966f85a08a5253
Nimesh Patel <nimesh.patel16@hotmail.com>
2018-02-16 16:05:58 +0530

    NP Checkin 16/2/18 4.05 PM

--- TaskManagementSystem/App_Start/BundleConfig.cs
+++ TaskManagementSystem/App_Start/BundleConfig.cs
@@ -8,8 +8,12 @@ namespace TaskManagementSystem
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+         
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -23,16 +27,10 @@ namespace TaskManagementSystem
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-          
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-         "~/Scripts/jquery-ui-{version}.js"));
-
-            bundles.Add(new StyleBundle("~/Content/cssjqryUi").Include(
-                   "~/Content/themes/base/jquery-ui.css"));
+            bundles.Add(new StyleBundle("~/Content/jqueryui").Include("~/Content/themes/base/all.css"));
         }
-    }
+}
 }
