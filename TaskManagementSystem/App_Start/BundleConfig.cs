fe0e417de51a0ee45cfad9457b4081ad9c00beaa
Nimesh Patel <nimesh.patel16@hotmail.com>
2018-02-16 14:05:53 +0530

    Pushing changes - NP 02/16

--- TaskManagementSystem/App_Start/BundleConfig.cs
+++ TaskManagementSystem/App_Start/BundleConfig.cs
@@ -23,9 +23,16 @@ namespace TaskManagementSystem
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+          
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+         "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new StyleBundle("~/Content/cssjqryUi").Include(
+                   "~/Content/themes/base/jquery-ui.css"));
         }
     }
 }
